63bb33f158af29300772b65e07ade20a78cba46b
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-03-26 20:06:39 -0700

    setup for sign and signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { auth } from "@/auth";
+import Profile from "@/components/profile";
 import * as actions from "@/actions";
 
+export default async function Home() {
+  const session = await auth();
 
-export default function Home() {
   return (
     <div>
       <form action={actions.signIn}>
-        <Button type="submit">Click me!!!</Button>
+        <Button type="submit">Sign In</Button>
       </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Signed Out</div>
+      )}
+
+      <Profile />
     </div>
   );
 }
---
+++ src/app/topics/[slug]/posts/new/page.tsx
@@ -0,0 +1,8 @@
+export default function PostCreatePost() {
+  return (
+    <div>
+      <h1>Post Create Page</h1>
+      <p>This is the post create page.</p>
+    </div>
+  );
+}
